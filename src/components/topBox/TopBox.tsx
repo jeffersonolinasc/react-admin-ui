1e75b5bccf4156e6111b949b072022975c3669dd
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2023-10-06 07:50:11 -0300

    graficos

--- src/components/topBox/TopBox.tsx
+++ src/components/topBox/TopBox.tsx
@@ -1,9 +1,28 @@
 import './topbox.scss'
-import {topDealUsers} from "../../data.ts"
+import { topDealUsers } from "../../data.ts"
 
 const TopBox = () => {
   return (
-    .topBox
+    <div className="topBox">
+      <h1>Top Deals</h1>
+      <div className="list">
+        {
+          topDealUsers.map((user) => (
+            <div key={user.id} className="listItem">
+
+              <div className="user">
+                <img src={user.img} alt="Perfil" />
+                <div className="userTexts">
+                  <span className="username">{user.username}</span>
+                  <span className="email">{user.email}</span>
+                </div>
+              </div>
+              <div className="amount">{user.amount}</div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
